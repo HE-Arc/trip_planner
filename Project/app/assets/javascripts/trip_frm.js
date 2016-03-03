440cf3baeb6f4e9fc8040c2acf58dea24866120e
Steve Visinand <visinandst@gmail.com>
2016-03-03 18:06:52 +0100

    finding adresses with maps API OK

--- Project/app/assets/javascripts/trip_frm.js
+++ Project/app/assets/javascripts/trip_frm.js
@@ -12,13 +12,24 @@ $( document ).ready(function() {
 function initGeo() {
     var geocoder = new google.maps.Geocoder();
 
-    $('#stage_adress').focusout(function() {
+    $('#find_addr').focusout(function() {
+        geocodeAddress(geocoder);
+    });
+}
+
+function initGeoEdit() {
+    var geocoder = new google.maps.Geocoder();
+
+    //init load
+    geocodeAddress(geocoder);
+
+    $('#find_addr').focusout(function() {
         geocodeAddress(geocoder);
     });
 }
 
 function geocodeAddress(geocoder) {
-    var address = document.getElementById('stage_adress').value;
+    var address = document.getElementById('find_addr').value;
     geocoder.geocode({'address': address}, function(results, status) {
         if (status === google.maps.GeocoderStatus.OK) {
             //resultsMap.setCenter(results[0].geometry.location);
@@ -27,9 +38,33 @@ function geocodeAddress(geocoder) {
             //  position: results[0].geometry.location
             //});
 
-            $('#info_adress').html("<b>" + results[0].formatted_address + "</b><br />GPS : " + results[0].geometry.location );
+            var htmlcode = "";
+            for(var i=0; i<results.length; i++){
+                var line = "<div class='formated_adress'><span class='rbtn_adress_span'>";
+                line += "<input class=\"rbtn\" type=\"radio\" name=\"stage[adress]\" value=\""+results[i].formatted_address+"\"";
+
+                if(i==0){
+                    line += " checked ";
+                }
+
+                line += "/></span><span><p>" + results[i].formatted_address+ "<br /><span class='gps_adress'>GPS:" + results[i].geometry.location + "</span></p></span>";
+                line += "<div>";
+                //var line = "<b>" + results[i].formatted_address + "</b><br />GPS : " + results[i].geometry.location + "<br/>";
+                htmlcode += line;
+            }
+
+            $('#finded_adresses').html(htmlcode);
+            $('#nb_results').text(results.length+" results")
+
         } else {
-            $('#info_adress').html(status);
+
+            if(status == 'ZERO_RESULTS'){
+                $('#nb_results').text(0+" result")
+            }
+            else{
+                $('#nb_results').text(status);
+            }
+
         }
     });
 }
