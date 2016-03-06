5e5f726dc5ba03e6a30f32295074171e0268eeb9
Steve Visinand <visinandst@gmail.com>
2016-03-06 20:07:01 +0100

    tests for models : stages, stage_types

--- Project/test/models/stage_test.rb
+++ Project/test/models/stage_test.rb
@@ -1,8 +1,26 @@
 require 'test_helper'
 
 class StageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-  
+
+  test "invalid without a title" do
+    stage = stages(:petite_promenade)
+    stage.title = nil
+    assert !stage.valid?, "Validation of the stage doesn't work"
+  end
+
+  test "valid with all values" do
+    stage = stages(:petite_promenade)
+    assert stage.valid?, 'Stage is not valid'
+  end
+
+  test "should have stagetype" do
+    stage = stages(:petite_promenade)
+    assert !stage.stagetype.nil?, 'Missing stagetype'
+  end
+
+  test "should belongs to one trip" do
+    stage = stages(:petite_promenade)
+    assert !stage.trip.nil?, "Stage hasn't a trip"
+  end
+
 end
--- Project/test/models/stagetype_test.rb
+++ Project/test/models/stagetype_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class StagetypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "invalid without a title" do
+    stagetype = stagetypes(:randonnee)
+    stagetype.title = nil
+    assert !stagetype.valid?, "Validation of the title of stagetype doesn't work"
+  end
+
+  test "invalid without a icon" do
+    stagetype = stagetypes(:randonnee)
+    stagetype.icon = nil
+    assert !stagetype.valid?, "Validation of the icon of stagetype doesn't work"
+  end
+
+  test "valid with all values" do
+    stagetype = stagetypes(:randonnee)
+    assert stagetype.valid?, 'Stagetype is not valid'
+  end
+
 end
