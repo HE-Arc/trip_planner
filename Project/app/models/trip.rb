12dbe957a24bd6968e5c7efb76bb649f8ebc05be
Margaux Divernois <margaux.divernois@gmail.com>
2016-03-06 19:39:46 +0100

    Ajout des premiers tests pour les voyages Ajout de valeurs de Fixtures
    Départ de l'issue #1 (toujours en cours de développement)

--- Project/app/models/trip.rb
+++ Project/app/models/trip.rb
@@ -1,9 +1,11 @@
 class Trip < ActiveRecord::Base
   has_many :stages
+  belongs_to :user
 
   validates :title,
             presence: true,
             length: {minimum: 0, maximum: 255}
   validates :description,
             length: {minimum: 0, maximum: 65535}
+
 end
--- Project/test/models/trip_test.rb
+++ Project/test/models/trip_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class TripTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "invalid without a title" do
+    t = trips (:myTrip)
+    t.title = nil
+    assert !t.valid?, "Title is not being validated"
+  end
+
+  test "valid with all attributes" do
+    t = trips(:myTrip)
+    assert t.valid?, "Trip is not valid"
+  end
+
+  test "should contain only stage that belong to trip" do
+    t = trips(:myTrip)
+    assert t.stages.all? {|s| s.trip == t }
+  end
+
+  test "should have one user" do
+    t = trips(:myTrip)
+    assert t.user
+  end
+
 end
